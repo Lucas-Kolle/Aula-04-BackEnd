cbcd3e765500b768f90ec39255f5b21ba596a13f
LUCAS DIAS BRANDAO KOLLE <25203704@CFP127ALUNO.LOCAL>
2026-03-13 11:28:50 -0300

    aula finalizada

--- modulo/array_json.js
+++ modulo/array_json.js
@@ -35,6 +35,7 @@ const listaDeFornecedores = []
 
 //função para exibir os dados do ARRAY
 const exibirDados = function(){
+
     //Exibe o objeto array e seu conteúdo no terminal
     console.log(listaDeNomes)
 
@@ -97,6 +98,7 @@ const exibirDados = function(){
 
 //funçaõ para manipular os dados
 const manipularDados = function(){
+
     //Adicionando valores novos novos no ARRAY através do indice
     console.log("\n********* FORMA MANUAL *********")
     listaClientes[0] = "José da Silva"
@@ -168,6 +170,7 @@ const removerItem = function(nome){
 
 //função para verificar a existencia de um item no ARRAY
 const verificarItem = function(nome){
+
     //Verifica a existencia de um conteúdo de uma lista (retorna TRUE / FALSE)
     return listaDeNomes.includes(nome)
 }
@@ -184,4 +187,115 @@ const quantidadeItens = function(nome){
     return cont
 }
 
-manipularDados()
+//função que cria dados do Json
+const criandoDadosJson = function(){
+
+    //criando e adicionando os dados do JSON
+    console.log("\n********* EXIBIR O OBJETO JSON COMPLETO *********")
+
+    let aluno = {"nome": "José", "ra": 123456, "telefone": "9725367839", "email": "josé@gmail.com"}
+    console.log(aluno)
+    console.table(aluno)
+
+    //mostrando um atributo específico
+    console.log("\n********* EXIBIR UM ATRIBUTO ESPECÍFICO *********")
+    console.log(aluno.nome)
+    console.log(aluno.email)
+
+    //adcionando um atributo após o objeto criado
+    console.log("\n********* ADICIONA UM ATRIBUTO NOVO *********")
+    aluno.sexo = "Masculino"
+    console.log(aluno)
+
+    //apagando um atributo específico do objeto
+    console.log("\n********* APAGA UM ATRIBUTO ESPECÍFICO *********")
+    delete aluno.telefone
+    console.log(aluno)
+}
+
+//função para cadastrar produtos 
+const cadastroDeProdutos = function(){
+
+    //criando o objeto ARRAY que guarda as cores do produto em JSON
+    let cores = [
+        {"id": 1, "cor": "Branco"}, //Indice 0
+        {"id": 2, "cor": "Preto"}, //Iindice 1
+        {"id": 3, "cor": "Azul"}, //Iindice 2
+        {"id": 4, "cor": "Rosa"}, //Iindice 3
+        {"id": 5, "cor": "Cinza"} //Iindice 4
+    ]
+
+    //criando o objeto ARRAY que vai guardar as marcas do produto em JSON
+    let marcas = [
+        {"id": 1, "marca": "LG",            "telefone": "343456758",    "email": "lg@gmail.com"}, //Indice 0
+        {"id": 2, "marca": "Dell",          "telefone": "282837494",    "email": "dell@gmail.com"}, //Indice 1
+        {"id": 3, "marca": "Lenovo",        "telefone": "396958378",    "email": "lenovo@gmail.com"}, //Indice 2
+        {"id": 4, "marca": "Apple",         "telefone": "099498576",    "email": "apple@gmail.com"}, //Indice 3
+        {"id": 5, "marca": "Rayzer",        "telefone": "384958673",    "email": "rayzer@gmail.com"}, //Indice 4
+        {"id": 6, "marca": "Logitech",      "telefone": "738495039",    "email": "logitech@gmail.com"}, //Indice 5
+        {"id": 7, "marca": "Multilaser",    "telefone": "73847586",     "email": "multilaser@gmail.com"} //Indice 6
+    ]
+
+    let produtos = [
+        {
+            "id": 1, 
+            "nome": "Monitor", 
+            "descricao": "27 polegadas",
+            "marca": [
+                marcas[2].marca
+            ],
+            "qtde": 20,
+            "cor": [ // mandadndo o objeto JSON completo 
+                cores[4],
+                cores[1]
+            ],
+            "valor": 880.50
+        },
+
+        {
+            "id": 2,
+            "nome": "Teclado",
+            "descricao": "Teclado mecânico RGB",
+            "marca": [
+                marcas[0].marca
+            ],
+            "qtde": 200,
+            "cor": cores,
+            "valor": 150
+        },
+
+        {
+            "id": 3,
+            "nome": "Mouse",
+            "descricao": "Mouse sem fio",
+            "marca": [
+                marcas[0].marca,
+                marcas[1].marca,
+                marcas[5].marca,
+            ],
+            "qtde": 500,
+            "cor": [
+                cores[0],
+                cores[1],
+                cores[4],
+            ],
+            "valor": 80
+        }
+    ]
+
+
+    produtos.forEach(function(nomeProduto){
+        
+
+        nomeProduto.marca.forEach(function(marcaProduto){
+            
+        })
+
+        nomeProduto.cor.forEach(function(corProduto){
+        
+        })
+    })
+
+}
+
+cadastroDeProdutos()
